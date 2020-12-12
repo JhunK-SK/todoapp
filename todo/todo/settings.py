820a3334a6776a606f80f623f47a11e2d3adcedf
Jeonghun <kim.jeonghun88@gmail.com>
2020-12-12 22:17:18 +0100

    updated static files

--- todo/todo/settings.py
+++ todo/todo/settings.py
@@ -122,8 +122,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIR = [
-    os.path.join(BASE_DIR, 'static'),
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
 ]
 
 MEDIA_URL = '/images/'
